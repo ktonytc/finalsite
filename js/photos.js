76c5e17e10ff119d05d3e4a51fc92bd8835ee8bc
Christina <christina_tonisheva@ukr.net>
2016-06-08 13:20:53 +0000

    Message

---
+++ js/photos.js
@@ -0,0 +1,59 @@
+body{
+    background-image: url("stone.jpg");
+	width: 1340px;
+	height: 1640px;
+	margin: 0 auto;
+}
+
+
+
+ul.menu{
+	width: 630px;
+	position: absolute;
+	top: 30px;
+	right: 100px;
+}
+
+ul.menu li{
+		list-style: none;
+		display: inline-block;
+		
+}
+
+ul.menu li a{
+	display: block;
+	background: Dimgray;
+	color: white;
+	text-decoration: none;
+	width: 140px;
+	text-align: center;
+	padding: 10px 0;
+
+	transition: 0.5s;
+}
+
+ul.menu li a:hover{
+	color: black;
+	background: white;	
+}
+
+ul.spusok{
+	width: 300px;
+	
+}
+
+.text{
+	display: block;
+	color: white;
+	text-align: center;
+	font-family: italic;
+	font-size: 26px;
+	position: absolute;
+	right: 120px;
+	top: 50px;
+	width: 800px;
+	height: 420px;
+	border-radius: 30px;
+}
+
+	
