3f5b587f3d6a8943c3c5b6ef95ae5aab1a97b0df
Christina <christina_tonisheva@ukr.net>
2016-06-12 15:21:17 +0000

    Message

--- js/photos.js
+++ js/photos.js
@@ -2,23 +2,23 @@
 	var image = document.getElementById("image");
 	
 	if(image.src.match("1")){
-		image.src = "7.jpg";
+		image.src = "7.JPG";
 		
 	} else if(image.src.match("2")){
-		image.src = "3.jpg";
+		image.src = "3.JPG";
 	} else if( image.src.match("3")){
-		image.src = "4.jpg";
+		image.src = "4.JPG";
 	} else if( image.src.match("4")){
-		image.src = "5.jpg";
+		image.src = "5.JPG";
 	} else if( image.src.match("5")){
-		image.src = "6.jpg";
+		image.src = "6.JPG";
 	} else if( image.src.match("6")){
-		image.src = "7.jpg";
+		image.src = "7.JPG";
 	} else if( image.src.match("7")){
-		image.src = "9.jpg";
+		image.src = "9.JPG";
 	} else if( image.src.match("9")){
-		image.src = "10.jpg";
+		image.src = "10.JPG";
 	} else {
-		image.src = "1.jpg";
+		image.src = "1.JPG";
 	}
 }
